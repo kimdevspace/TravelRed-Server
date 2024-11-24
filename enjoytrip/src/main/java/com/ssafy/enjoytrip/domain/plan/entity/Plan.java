febb586c4b351e3413f26a2ef001dcaba2ae1030
kimdevspace <ejk5148@gmail.com>
2024-11-25 02:17:53 +0900

    fix: plan 관련 엔터티 수정

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/plan/entity/Plan.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/plan/entity/Plan.java
@@ -2,53 +2,62 @@ package com.ssafy.enjoytrip.domain.plan.entity;
 
 import com.ssafy.enjoytrip.domain.city.entity.City;
 import com.ssafy.enjoytrip.domain.member.entity.Member;
-import com.ssafy.enjoytrip.domain.tour.entity.TourContent;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
-import static lombok.AccessLevel.PROTECTED;
-
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor(access = PROTECTED)
-@Table(name = "plan")
+@Entity //이 클래스가 JPA 엔터티임을 나타낸다. DB 테이블과 매핑
+@Table(name = "plan") // 매핑할 테이블의 이름 지정
+@Getter // 롬복, 모든 필드의 게터 메서드 생성
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 롬복, 파라미터가 업슨ㄴ 기본생성자 생성 (액세스 레벨을 설정하여 외부에서 무분별한 객체생성 막기)
 public class Plan {
-    @Id
+
+    @Id //기본키
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment 사용함을 의미
     @Column(name = "plan_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long planId;
+    private Long id;
+
+    @Column(name = "plan_title", nullable = false)
+    private String title;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
+    @JoinColumn(name = "member_id")
     private Member member;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "city_code", nullable = false)
-    private City city;
-
-    @NotNull
-    private String planTitle;
-
+    @Column(name = "thumbnail_image")
     private String thumbnailImage;
 
+    @Column(name = "start_date")
     private LocalDate startDate;
+
+    @Column(name = "end_date")
     private LocalDate endDate;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "city_code")
+    private City city;
+
+    @Column(name = "day", nullable = false)
+    private Integer day;
+
     @Builder
-    public Plan(Member member, City city, String planTitle,
-                String thumbnailImage, LocalDate startDate, LocalDate endDate) {
+    private Plan(String title, Member member, String thumbnailImage,
+                 LocalDate startDate, LocalDate endDate, City city, Integer day) {
+        // 필수값 검증
+        if (title == null || member == null || city == null || day == null) {
+            throw new IllegalArgumentException("필수 값이 누락되었습니다.");
+        }
+
+        this.title = title;
         this.member = member;
-        this.city = city;
-        this.planTitle = planTitle;
         this.thumbnailImage = thumbnailImage;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.city = city;
+        this.day = day;
     }
 }
