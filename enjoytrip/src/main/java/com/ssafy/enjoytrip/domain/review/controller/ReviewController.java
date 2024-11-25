1fd8c0968a8d554d9619a052a9495a8759a31dfe
yuri <yetcome0123@gmail.com>
2024-11-25 13:27:42 +0900

    feat: 관광지별 리뷰 조회 기능 추가

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/review/controller/ReviewController.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/review/controller/ReviewController.java
@@ -1,14 +1,12 @@
 package com.ssafy.enjoytrip.domain.review.controller;
 
 import com.ssafy.enjoytrip.domain.review.dto.request.CreateReviewRequestDto;
-import com.ssafy.enjoytrip.domain.review.entity.Review;
+import com.ssafy.enjoytrip.domain.review.dto.response.ReviewResponseDto;
+import com.ssafy.enjoytrip.domain.review.dto.response.ReviewSummaryDto;
 import com.ssafy.enjoytrip.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +19,17 @@ public class ReviewController {
     public ResponseEntity<?> createReview(@RequestBody CreateReviewRequestDto reviewRequestDto) {
         CreateReviewRequestDto result = reviewService.saveReview(reviewRequestDto);
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> verifyToken() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{tourId}")
+    public ResponseEntity<?> getReview(@PathVariable Long tourId) {
+        ReviewResponseDto reviewResponseDto = reviewService.getReview(tourId);
+        return ResponseEntity.ok(reviewResponseDto);
     }
 
 
