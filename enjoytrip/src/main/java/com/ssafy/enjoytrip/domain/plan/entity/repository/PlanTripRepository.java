82cec2077c54637a0fc66bc0c73b9f4f9cee1102
kimdevspace <ejk5148@gmail.com>
2024-11-25 15:01:33 +0900

    feat: 상세여행계획조회 기능 추가

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/plan/entity/repository/PlanTripRepository.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/plan/entity/repository/PlanTripRepository.java
@@ -1,9 +1,20 @@
 package com.ssafy.enjoytrip.domain.plan.entity.repository;
 
+import com.ssafy.enjoytrip.domain.plan.entity.Plan;
 import com.ssafy.enjoytrip.domain.plan.entity.PlanTrip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlanTripRepository extends JpaRepository<PlanTrip, Long> {
+    @Query("SELECT pt FROM PlanTrip pt " +
+            "JOIN FETCH pt.tour t " +
+            "JOIN FETCH t.tourDetail td " +
+            "WHERE pt.plan = :plan " +
+            "ORDER BY pt.day ASC, pt.order ASC")
+    List<PlanTrip> findByPlanOrderByDayAscOrderAsc(@Param("plan") Plan plan);
 }
